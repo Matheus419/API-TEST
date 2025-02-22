372516c2c7458aca2851c93566fc73122b924425
MATHEUS HENRIQUE BATISTA PEREIRA <matheus.hbpereira1@senacsp.edu.br>
2025-02-21 22:16:24 -0300

    Primeiro commit 2.0

---
+++ backend/model/Adress.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+// Define o modelo
+const addressSchema = mongoose.Schema({
+    
+    cep: {
+        type: String,
+        required: true,
+        unique: true, // Garante que o Cep seja unico registro 
+    },
+
+    longradouro: {
+        type: String,
+        required: true
+    },
+
+    bairro: {
+        type: String,
+        required: true
+    },
+
+    cidade: {
+        type: String,
+        required: true,
+    },
+
+    estado: {
+        type: String,
+        required: true,
+        MaxLength: 2, // Estado deve ter no maximo 2 caracter EX: SP
+    },
+}, {
+    timeStamp: true // Adiciona hora da criação e edição
+});
+
+// Cria o modelo
+const Address = mongoose.model('Address', addressSchema);
+
+
+// Exporta o modelo para ser usado
+module.exports = Address;
+
---
+++ backend/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const axios = require('axios');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Adress = require ("../backend/model/Adress");
+
+
+// Carrega as variáveis de ambiente .ENV
+dotenv.config();
+
+// Cria uma instância do Express -> Servidor
+const app = express();
+
+app.use((req, res, next) => {
+    res.header("Acess-Control-Allow-Origin", "*"); // Permite qualquer origem para req.
+    res.header("Acess-Control-Allow-Methods", "GET", "POST"); // Permite apenas métodos GET e POST
+    res.header("Acess-Control-Allow-Headers", "Content-Type"); // Permite o cabeçalho nas req.
+    next();
+});
+
+
+// JSON nas requisições / Config do Express
+app.use(express.json());
+
+app.get('/api/cep/:cep/json', async (req, res) => {
+    const { cep } = req.params; // Extrai o Cep
+
+    try { 
+        // Requisição GET para API ViaCep, passando um Cep
+        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json`)
+        res.json(response.data); // Retorna da API a resposta conforme o CEP
+    } catch (error) {
+        res.status(500).json({error: "Erro ao buscar o CEP!"}); // Em caso de erro
+    }
+});
+
+// Obtem as variaveis do .ENV
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS;
+
+// Define o Link
+const mongoURI = `mongodb+srv://${dbUser}:${dbPassword}@clusterapi.1j8mh.mongodb.net/?retryWrites=true&w=majority&appName=ClusterAPI`
+
+// Porta que roda o servidor
+const port = 3000;
+
+mongoose
+    .connect(mongoURI)  // Conecta ao banco de dados com o Link gerado
+    .then(() => { // Quando for conexão bem sucedida
+        console.log('Conectou ao Banco')
+        app.listen(port, () => {
+            console.log(`Servidor rodando em http://localhost:${port}`);
+        });
+    })
+    .catch((err) => console.log("Erro ao conectar ao MongoDB", err))
